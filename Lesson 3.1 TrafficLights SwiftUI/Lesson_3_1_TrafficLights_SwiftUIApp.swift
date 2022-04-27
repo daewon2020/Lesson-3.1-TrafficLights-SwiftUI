5356a342a6bc024c812bf78eeacde976f40b3a7c
Kostya Andreev <daewon@yandex.ru>
2022-04-27 22:43:51 +0300

    Initial Commit

---
+++ Lesson 3.1 TrafficLights SwiftUI/Lesson_3_1_TrafficLights_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_1_TrafficLights_SwiftUIApp.swift
+//  Lesson 3.1 TrafficLights SwiftUI
+//
+//  Created by Константин Андреев on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_1_TrafficLights_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
